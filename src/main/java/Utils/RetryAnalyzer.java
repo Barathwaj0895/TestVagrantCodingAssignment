dd930daeb3d27521d1cd2a0a26c13b99525a85d2
Ravisankar, Barathwaj <barathwaj.ravisankar@boeing.com>
2023-12-18 22:51:33 +0530

    updated the class with workflow explanations

--- src/main/java/Utils/RetryAnalyzer.java
+++ src/main/java/Utils/RetryAnalyzer.java
@@ -6,6 +6,7 @@ import org.testng.ITestResult;
 
 /**
  * This class has been created by Barathwaj Ravisankar on May 12, 2022
+ * This class has been modified by Barathwaj Ravisankar on December 18, 2023
  */
 public class RetryAnalyzer implements IRetryAnalyzer {
     int count = 0;
@@ -15,8 +16,10 @@ public class RetryAnalyzer implements IRetryAnalyzer {
     public boolean retry(ITestResult iTestResult) {
         while (count < retryCount) {
             count++;
+            // Allow for multiple retries
             return true;
         }
+        // Retry count exceeded, return false
         return false;
     }
 }
